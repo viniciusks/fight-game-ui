b7d226c5d58b796d60682710d6b18d105b005c42
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2019-04-03 09:53:14 -0300

    Dados jogador adicionados

---
+++ src/components/dadosJogador/DadosJogador.vue
@@ -0,0 +1,16 @@
+<template src="./dadosJogador.pug" lang="pug"></template>
+
+<script>
+export default {
+    data() {
+        return {
+            nome: "Kremer",
+            nivel: 5
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/login/Login'
 import Cadastro from '@/components/cadastro/Cadastro'
+import DadosJogador from '@/components/dadosJogador/DadosJogador'
 import Teste from '@/components/teste/Teste'
 
 Vue.use(Router)
@@ -18,6 +19,11 @@ export default new Router({
       path: '/cadastro',
       name: 'Cadastro',
       component: Cadastro
+    },
+    {
+      path: '/dados-jogador',
+      name: 'Dados Jogador',
+      component: DadosJogador
     },
     {
       path: '/teste',
